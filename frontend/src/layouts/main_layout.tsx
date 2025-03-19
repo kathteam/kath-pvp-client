33a4fca9dd549cfbe287d2aa6fb276712a0cc1fc
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2025-03-19 20:51:58 +0200

    feat(theme)-integrated_brand_logo_with_text

--- frontend/src/layouts/main_layout.tsx
+++ frontend/src/layouts/main_layout.tsx
@@ -1,11 +1,23 @@
 import { JSX } from "react";
 import { Outlet } from "react-router-dom";
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import { LogoIcon, TitleIcon} from "@/components/icons";
+
+function customAppTitle(): JSX.Element {
+  return (
+    <Stack direction='row' alignItems='center' spacing={2} px={0.5}>
+      <LogoIcon />
+      <TitleIcon />
+    </Stack>
+  )
+}
 
 export default function MainLayout(): JSX.Element {
   return (
-    <DashboardLayout>
+    <DashboardLayout slots={{
+      appTitle: customAppTitle,
+    }}>
       <Box sx={{ flex: 1, border: 0}}>
         <Outlet />
       </Box>

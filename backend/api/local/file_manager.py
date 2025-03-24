4188d3cffc9a35d867aa8f9e8d8fe98097cf84f6
nojux-official <nojus.sajauskas@gmail.com>
2025-03-24 22:33:21 +0200

    Pylint issues

--- backend/api/local/file_manager.py
+++ backend/api/local/file_manager.py
@@ -1,11 +1,8 @@
+import os
 from logging import Logger
-
 from utils.logger import get_logger
 
-import os
-
 WORKDIR = os.path.join(os.path.expanduser("~"), "Documents")
-
 
 class FileManager:
     def __init__(self) -> None:
